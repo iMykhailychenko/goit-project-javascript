06422ae7fb6c7b86e09449b22bf60b27ff912289
imykhailychenko <igor.c.m@ukr.net>
2020-01-12 11:36:23 +0200

    search done

--- src/js/scripts/searchFilm.js
+++ src/js/scripts/searchFilm.js
@@ -37,14 +37,41 @@ class Search {
   fetchResoult(calback) {
     const way = 'search';
     const param = this.getSearchParam.bind(this);
+    if (param() === undefined) {
+      calback(undefined);
+      return;
+    }
     fetch.getFilmsList(way, param).then(data => {
       calback(data);
     });
   }
 
   renderList(data) {
-    console.log('найдено ' + data.total_results + ' фильмов');
-    console.log(data.results);
+    const searchBlock = document.querySelector('.results');
+    const searchTitle = document.querySelector('.results__title');
+    const searchList = document.querySelector('.results__list');
+
+    if (data === undefined) {
+      searchTitle.textContent = '';
+      searchList.innerHTML = '';
+      searchBlock.hidden = true;
+      return;
+    }
+
+    const firstFilms = data.results;
+    firstFilms.length = 8;
+    searchBlock.hidden = false;
+    searchTitle.textContent = `найдено  ${data.total_results} фильмов`;
+
+    const markup = firstFilms
+      .map(item => {
+        return `<li class="results__item">
+                <p class="results__text">${item.title}</p>
+              </li>`;
+      })
+      .join('');
+    searchList.innerHTML = '';
+    searchList.insertAdjacentHTML('beforeend', markup);
   }
 
   renderFilms(data) {
